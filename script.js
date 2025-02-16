820824cfcda04ffbf906b12b2491ad1f8ce710ad
ymicron <165476073+ymicron@users.noreply.github.com>
2025-02-16 08:47:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Hämta referenser till element
+const languageBtn = document.getElementById('language-btn');
+const dropdownMenu = document.getElementById('language-dropdown').querySelector('.dropdown-menu');
+
+// Lägg till en event listener på språkknappen för att växla synligheten på menyn
+languageBtn.addEventListener('click', function(event) {
+    event.preventDefault(); // Förhindra att knappen gör något annat, t.ex. ladda om sidan
+    
+    // Växla synligheten på menyn
+    dropdownMenu.style.display = (dropdownMenu.style.display === 'block') ? 'none' : 'block';
+});
+
+// Stäng dropdown menyn om användaren klickar utanför
+window.addEventListener('click', function(event) {
+    if (!languageBtn.contains(event.target) && !dropdownMenu.contains(event.target)) {
+        dropdownMenu.style.display = 'none'; // Stänger menyn om man klickar utanför
+    }
+});
